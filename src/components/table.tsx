4a2d3ba241f4b7e793410a76153da66124a56366
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-03-30 23:59:41 +0800

    create table component for showing data

---
+++ src/components/table.tsx
@@ -0,0 +1,43 @@
+import DataTable from "react-data-table-component";
+
+const TableData = () => {
+  const columns = [
+    {
+      name: "Title",
+      selector: (row) => row.title,
+    },
+    {
+      name: "Year",
+      selector: (row) => row.year,
+    },
+  ];
+
+  const data = [
+    {
+      id: 1,
+      title: "Beetlejuice",
+      year: "1988",
+    },
+    {
+      id: 2,
+      title: "Ghostbusters",
+      year: "1984",
+    },
+  ];
+
+  return (
+    <DataTable
+      columns={columns}
+      data={data}
+      persistTableHead
+      // onAddSchedule={addSchedule}
+      // pagination
+      // paginationPerPage={10}
+      // paginationComponentOptions={paginationComponentOptions}
+      // actions={actionsMemo}
+      // noDataComponent={noDataComponent}
+    />
+  );
+};
+
+export default TableData;
